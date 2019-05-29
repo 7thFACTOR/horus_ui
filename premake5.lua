0597f533f8b37bfee54de84b240b50d755b0b27d
Nicusor Nedelcu <nekitu@gmail.com>
2019-05-29 23:44:27 +0300

    removed freetype for now

--- premake5.lua
+++ premake5.lua
@@ -1,114 +1,115 @@
+require("vstudio")
 require('remake')
 workspace "horus"
-	configurations { "Debug", "Development", "Release" }
-	
-	-- global options
-	architecture "x64" -- We only support 64bit architectures
-	characterset "Unicode" -- We support Unicode
-	flags { "NoMinimalRebuild", "MultiProcessorCompile", "NoPCH" }
+configurations { "Debug", "Development", "Release" }
 
-	-- build config
-	if _ACTION == "vs2015" or _ACTION == "vs2017" then
-		system "windows"
-	elseif _ACTION == "gmake" then
-		if _ARGS[1] == "macos" then
-			system "macosx"
-		else
-			system "linux"
-		end
-	elseif _ACTION == "xcode" then
-		system "macosx"
-	else
-		premake.error("Unknown/Unsupported build action: " .. _ACTION)
-	end
+-- global options
+architecture "x64" -- We only support 64bit architectures
+characterset "Unicode" -- We support Unicode
+flags { "NoMinimalRebuild", "MultiProcessorCompile", "NoPCH" }
 
-	paths = Config('src', 'libs', ('build_' .. _ACTION), 'bin')
+-- build config
+if _ACTION == "vs2015" or _ACTION == "vs2017" then
+  system "windows"
+elseif _ACTION == "gmake" then
+  if _ARGS[1] == "macos" then
+    system "macosx"
+  else
+    system "linux"
+  end
+elseif _ACTION == "xcode" then
+  system "macosx"
+else
+  premake.error("Unknown/Unsupported build action: " .. _ACTION)
+end
 
-	--- prepare naming convention for library files
-	filter "kind:SharedLib or StaticLib"
-		pic "on"
+paths = Config('src', 'libs', ('build_' .. _ACTION), 'bin')
 
-	filter { "kind:SharedLib or *App", "configurations:Debug or Development" }
-		targetsuffix '_d'
+--- prepare naming convention for library files
+filter "kind:SharedLib or StaticLib"
+  pic "on"
 
-	filter { "kind:StaticLib", "configurations:Debug or Development" }
-		targetsuffix '_sd'
-		
-	--- prepare the configurations
-	filter "configurations:Debug"
-		symbols "On"
-		optimize "Debug"
-		defines "_DEBUG"
-		warnings "Off"
+filter { "kind:SharedLib or *App", "configurations:Debug or Development" }
+  targetsuffix '_d'
 
-	filter "configurations:Development"
-		symbols "On"
-		optimize "Speed"
-		defines { "_DEVELOPMENT", "NDEBUG" }
-		warnings "Off"
+filter { "kind:StaticLib", "configurations:Debug or Development" }
+  targetsuffix '_sd'
+  
+--- prepare the configurations
+filter "configurations:Debug"
+  symbols "On"
+  optimize "Debug"
+  defines "_DEBUG"
+  warnings "Off"
 
-	filter "configurations:Release"
-		symbols "Off"
-		optimize "Full"
-		flags "LinkTimeOptimization"
-		defines { "_SHIPPING", "_RELEASE", "NDEBUG" }
-		warnings "Off"
+filter "configurations:Development"
+  symbols "On"
+  optimize "Speed"
+  defines { "_DEVELOPMENT", "NDEBUG" }
+  warnings "Off"
 
-	filter { "system:windows", "action:vs*" }
-		systemversion(os.winSdkVersion() .. ".0")
-		
-	filter { "system:linux or macosx or ios" }
-		buildoptions { "-pthread", "-fpermissive" }
+filter "configurations:Release"
+  symbols "Off"
+  optimize "Full"
+  flags "LinkTimeOptimization"
+  defines { "_SHIPPING", "_RELEASE", "NDEBUG" }
+  warnings "Off"
 
-	filter "system:windows"
-		-- disable some warnings
-		disablewarnings { 4251, 4006, 4221, 4204 }
-		defines "_WINDOWS"
+filter { "system:windows", "action:vs*" }
+  systemversion(os.winSdkVersion() .. ".0")
+  
+filter { "system:linux or macosx or ios" }
+  buildoptions { "-pthread", "-fpermissive" }
 
-	filter "system:macosx or ios"
-		defines "_APPLE"
+filter "system:windows"
+  -- disable some warnings
+  disablewarnings { 4251, 4006, 4221, 4204 }
+  defines "_WINDOWS"
 
-	filter "system:linux"
-		defines "_LINUX"
+filter "system:macosx or ios"
+  defines "_APPLE"
 
-	filter{}
+filter "system:linux"
+  defines "_LINUX"
 
-	library("os", function()
-		if os.target() == "windows" then
-			public.links {
-				"shlwapi",  "Ws2_32", "Wininet", "dbghelp",
-				"user32", "gdi32", "ole32", "oleaut32",  "uuid"
-			}
-		elseif os.target() == "linux" then
-			public.links {
-				"X11 `pkg-config --libs gtk+-3.0`",
-				"Xi", "dl", "pthread", "Xext"
-			}
-		elseif os.target() == "macosx" then
-			public.links { 
-				"dl", "ForceFeedback.framework", "CoreVideo.framework", "Cocoa.framework",
-				"IOKit.framework", "Carbon.framework", "CoreAudio.framework", "AudioToolbox.framework",
-			}			
-		end
-	end)
+filter{}
 
-	library("opengl", function()
-		public.defines "USE_OPENGL"
+library("os", function()
+  if os.target() == "windows" then
+    public.links {
+      "shlwapi",  "Ws2_32", "Wininet", "dbghelp",
+      "user32", "gdi32", "ole32", "oleaut32",  "uuid"
+    }
+  elseif os.target() == "linux" then
+    public.links {
+      "X11 `pkg-config --libs gtk+-3.0`",
+      "Xi", "dl", "pthread", "Xext"
+    }
+  elseif os.target() == "macosx" then
+    public.links { 
+      "dl", "ForceFeedback.framework", "CoreVideo.framework", "Cocoa.framework",
+      "IOKit.framework", "Carbon.framework", "CoreAudio.framework", "AudioToolbox.framework",
+    }
+  end
+end)
 
-		if os.target() == "windows" then
-			public.links "opengl32"
-		elseif os.target() == "linux" then
-			public.links { "GL", "GLU", "X11" }
-		elseif os.target() == "macosx" then
-			public.links "OpenGL.framework"
-		end
-	end)
-	
-	--group "libs"
-	--	include "libs/all.lua"
+library("opengl", function()
+  public.defines "USE_OPENGL"
 
-	--group "horus_ui"
-		--include "src"
+  if os.target() == "windows" then
+    public.links "opengl32"
+  elseif os.target() == "linux" then
+    public.links { "GL", "GLU", "X11" }
+  elseif os.target() == "macosx" then
+    public.links "OpenGL.framework"
+  end
+end)
 
-	--group "examples"
-		--includeall "examples"
+group "libs"
+  include "libs/all.lua"
+
+group "horus_ui"
+  include "src"
+
+group "examples"
+  includeall "examples"
