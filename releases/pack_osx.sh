1ff66047e56cf9926233e57855e9f64f5bc44097
Nicusor Nedelcu <nekitu@gmail.com>
2019-05-30 23:05:50 +0300

    added release archive builders

---
+++ releases/pack_osx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export HORUS_VER=0_1_beta
+tar -cvf ./releases/archives/horus_ui_x64_osx_${HORUS_VER}.tar -T filelist_osx.txt
---
+++ releases/pack_windows.cmd
@@ -0,0 +1,3 @@
+cd ..
+set HORUS_VER=0_1_beta
+releases\\7z a releases/archives/horus_ui_x64_vs2017_%HORUS_VER%.zip @releases\\filelist_windows.txt
