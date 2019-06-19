9ec61059169466c05cf57ff2c85fd6024613f874
Nicusor Nedelcu <nekitu@gmail.com>
2019-06-19 13:55:35 +0300

    added the rmgui Osiris files

---
+++ src/osiris.cpp
@@ -0,0 +1,106 @@
+#include "osiris.h"
+#include "types.h"
+
+namespace hui
+{
+Element::Element()
+{
+
+}
+
+Element::~Element()
+{
+	deleteChildren();
+}
+
+bool Element::castRay(const Vec3& start, const Vec3& dir, Vec3& outIntersection, f32& outTimeOnRay)
+{
+
+	return false;
+}
+
+Element* Element::findElement(const std::string& elementName)
+{
+	if (elementName == name)
+	{
+		return this;
+	}
+
+	for (auto& iter : children)
+	{
+		auto el = iter->findElement(elementName);
+	
+		if (el)
+			return el;
+	}
+
+	return nullptr;
+}
+
+void Element::addChild(Element* element)
+{
+	auto iter = std::find(children.begin(), children.end(), element);
+
+	if (iter != children.end())
+		return;
+
+	element->parent = this;
+	children.push_back(element);
+}
+
+void Element::deleteChild(Element* element)
+{
+	auto iter = std::find(children.begin(), children.end(), element);
+
+	if (iter == children.end())
+		return;
+
+	delete (*iter);
+	children.erase(iter);
+}
+
+void Element::deleteChildren()
+{
+	for (auto& el : children)
+	{
+		delete el;
+	}
+
+	children.clear();
+}
+
+void Element::draw()
+{
+
+}
+
+void Element::update()
+{
+	worldTransform.reset();
+}
+
+ButtonElement::ButtonElement()
+{}
+
+void ButtonElement::update()
+{}
+
+void ButtonElement::draw()
+{}
+
+bool ButtonElement::onMouseDown(const InputEvent3D& ev)
+{}
+
+bool ButtonElement::onMouseUp(const InputEvent3D& ev)
+{}
+
+bool ButtonElement::onMouseMove(const InputEvent3D& ev)
+{}
+
+bool ButtonElement::onKeyDown(const InputEvent3D& ev)
+{}
+
+bool ButtonElement::onKeyUp(const InputEvent3D& ev)
+{}
+
+}
